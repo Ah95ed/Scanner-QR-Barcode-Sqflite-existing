9e53ac92ac0cf153f405f8228794d16fcfdb605b
Alqdees <amhmeed31@gmail.com>
2023-02-02 01:06:27 +0300

    add dialog to delelte

--- lib/model/User.dart
+++ lib/model/User.dart
@@ -11,30 +11,5 @@ class User {
     required this.id,
   });
 
-  // User.map(dynamic opj){
-  //   id=opj['id'];
-  //   name=opj["Name"];
-  //   barcode=opj["BarCode"];
-  //   cost=opj["Cost"];
-  //   sell=opj["Sell"];
-  //   // this.id=opj["ID"];
-  //   // dose=opj["dose"];
-  //   // mostSide=opj['mostSide'];
-  //   // drugName=opj['drugName'];
-  //   // pregnancy=opj['pregnancy'];
-  // }
-  // Map< String , dynamic > map(){
-  //   var map =  Map<String,dynamic>();
-  //   map['Name'] = name;
-  //   map['BarCode'] = barcode;
-  //   map['Cost'] = cost;
-  //   map['Sell'] = sell;
-  //   // map['dose'] = dose;
-  //   // map['mostSide'] = mostSide;
-  //   // map['drugName'] = drugName;
-  //   // map['pregnancy'] = pregnancy;
-  //
-  //   return map;
-  // }
 
 }
